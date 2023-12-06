63ce53171ca3b7e4077fb2625be9681ef0b837ec
Siddharth Borman <siddharth002b@gmail.com>
2023-12-06 11:04:13 +0530

    added project type

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import {getProjects} from '@/sanity/sanity-utlis'
+import { Project } from '@/types/Project';
+
 
 export default async function Home() {
 
   const projects = await getProjects();
 
   return (
-    <div>{projects.map((project)=>(
-      <div key={project._id}>{project.name}</div>
+    <div>
+      {projects.map((project)=>(
+      
+      <div key={project._id}>
+        {project.name}
+      </div>
+
     ))}
+
     </div>
-  )
+  );
 }
